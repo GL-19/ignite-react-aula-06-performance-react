4145979030d84f2892a9d7cfa7d1d43a16471818
GL-19 <loiolagl19@gmail.com>
2021-12-23 23:56:14 -0300

    Adicionada virtualização na listagem

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -2,6 +2,7 @@ import dynamic from "next/dynamic";
 import { memo, useState } from "react";
 import { Product } from "../types/Product";
 import { AddProductToWishListProps } from "./AddProductToWishList";
+import lodash from "lodash";
 
 const AddProductToWishList = dynamic<AddProductToWishListProps>(
 	() => {
@@ -35,5 +36,5 @@ function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
 }
 
 export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
-	return Object.is(prevProps.product, nextProps.product);
+	return lodash.isEqual(prevProps.product, nextProps.product);
 });
--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { List, ListRowRenderer } from "react-virtualized";
 import { Product } from "../types/Product";
 import { ProductItem } from "./ProductItem";
 
@@ -12,19 +13,26 @@ export function SearchResults({
 	totalPrice,
 	onAddToWishList,
 }: SearchResultsProps) {
+	const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+		return (
+			<div key={key} style={style}>
+				<ProductItem product={results[index]} onAddToWishList={onAddToWishList} />
+			</div>
+		);
+	};
+
 	return (
 		<div>
 			<h2>Valor total: R$ {totalPrice}</h2>
 
-			{results.map((product) => {
-				return (
-					<ProductItem
-						product={product}
-						key={product.id}
-						onAddToWishList={onAddToWishList}
-					/>
-				);
-			})}
+			<List
+				height={300}
+				rowHeight={30}
+				width={900}
+				overscanRowCount={5}
+				rowCount={results.length}
+				rowRenderer={rowRenderer}
+			/>
 		</div>
 	);
 }
